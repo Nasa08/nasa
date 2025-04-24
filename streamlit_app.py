ee47726976c0188e874a4239593b5b4563b437fe
Nasa08 <7h.nasa@gmail.com>
2025-04-24 13:42:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈nasayy")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.image("view/https://github.com/Nasa08/nasa/blob/main/views/IMG_20250424_124646.jpg")
+
