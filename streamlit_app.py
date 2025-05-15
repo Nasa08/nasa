6b129fe93a03e293b503900e160145120090e359
Nasa08 <7h.nasa@gmail.com>
2025-05-15 15:01:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
 st.title("🎈nasayy")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 st.image("IMG_20250514_115129.jpg", width=200)
 st.subheader("Hendri Setiadi, S.Tr.Kom")
-st.write(
-    "Mari latihan membuat Website melalui Github dan Streamli
-)
