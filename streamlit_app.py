29a96cb27da8f013ae5ceb1f6ce88f8f20abba85
Nasa08 <7h.nasa@gmail.com>
2025-05-15 14:54:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("🎈nasayy")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.image("views/IMG_20250514_115129.jpg", width=200)
+st.image("IMG_20250514_115129.jpg", width=200)
 st.subheader("Hendri Setiadi, S.Tr.Kom")
 st.write(
     "Mari latihan membuat Website melalui Github dan Streamli
