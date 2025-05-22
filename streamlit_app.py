dcf398db9305b86c60e4f8bf135743cf582572af
Nasa08 <7h.nasa@gmail.com>
2025-05-22 11:27:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,12 @@ import streamlit as st
 st.title("🎈nasayy")
 st.image("IMG_20250514_115129.jpg", width=200)
 st.subheader("Hendri Setiadi, S.Tr.Kom")
+
+#with col1:
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil")
